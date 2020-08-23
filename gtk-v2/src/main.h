be57e9061de4b9a0d55d23175aaffa1992053450
partmedia <partmedia@282e977c-c81d-0410-88c4-b93c2d0d6712>
2020-08-23 17:39:27 +0000

    Fix build with -fno-common
    window_xml_file appears in a header file without extern, causing a
    linker error when built with -fno-common.
    
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/client/trunk@21303 282e977c-c81d-0410-88c4-b93c2d0d6712

--- gtk-v2/src/main.h
+++ gtk-v2/src/main.h
@@ -34,7 +34,7 @@ extern int map_image_size, map_image_half_size, image_size;
 #define DIALOG_FILENAME CF_DATADIR "/ui/dialogs.ui"
 
 /** Path to the current UI file. */
-char window_xml_file[MAX_BUF];
+extern char window_xml_file[MAX_BUF];
 
 #define MAGIC_MAP_PAGE  1 /**< Notebook page of the magic map */
 
