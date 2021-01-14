754ffb9e102170bf301c62e81f50c40db44087f0
silvernexus <silvernexus@282e977c-c81d-0410-88c4-b93c2d0d6712>
2021-01-14 23:47:48 +0000

    Fix undefined variable in Big-Endian SDL code. Resolves bug #885
    git-svn-id: svn://svn.code.sf.net/p/crossfire/code/client/trunk@21770 282e977c-c81d-0410-88c4-b93c2d0d6712

--- gtk-v2/src/image.c
+++ gtk-v2/src/image.c
@@ -164,7 +164,7 @@ static void create_map_image(guint8 *data, PixmapInfo *pi) {
             *p = (g << 8) | (g << 16) | (g << 24) | *(l + 3);
         }
 
-        for (i=0; i < width * ny; i+= 4) {
+        for (i=0; i < width * height; i+= 4) {
             guint32 *tmp;
 
             /*
