3a24dcf84433516e6deef06ca003adddbcb8839e
Kevin Zheng <kevinz5000@gmail.com>
2024-04-14 00:43:17 -0700

    Update latency profiler to new client output format

--- utils/pprof-latency.py
+++ utils/pprof-latency.py
@@ -1,26 +1,40 @@
 """
 pprof-latency.py -- process Crossfire client latency profile
+usage:
+1. gather a profile using:
+
+   $ crossfire-client-gtk2 --profile-latency > out.txt
+
+2. process the profile:
+
+   $ python3 utils/pprof-latency.py out.txt
 """
 import sys
 
 def main():
-    data = sys.stdin.readlines()
-    fields = map(lambda l: l.strip().split(','), data)
-    pending = {}
-    for f in fields:
-        if f[0] == 'profile/com':
-            n, t, cmd = f[1:]
-            n = int(n)
-            t = int(t)
-            pending[n] = (t, cmd)
-        elif f[0] == 'profile/comc':
-            n, t, s, _ = f[1:]
-            n = int(n)
-            t = int(t)
-            tdiff = t - pending[n][0]
-            cmd = pending[n][1]
-            del pending[n]
-            print("%d,%s" % (tdiff, cmd))
+    if len(sys.argv) < 2:
+        print("usage: python3 pprof-latency.py FILE")
+        return
+
+    path = sys.argv[1]
+    with open(path, 'r') as dat, open(path + ".hist", 'w') as hist:
+        data = dat.readlines()
+        fields = map(lambda l: l.strip().split('\t'), data)
+        pending = {}
+        for f in fields:
+            if f[0] == 'profile/com':
+                n, cmd = f[1:]
+                n = int(n)
+                pending[n] = cmd
+            elif f[0] == 'profile/comc':
+                n, t, s, _ = f[1:]
+                n = int(n)
+                t = int(t)
+                tdiff = t
+                cmd = pending[n]
+                del pending[n]
+                print("%d,%s" % (tdiff, cmd))
+                print("%d" % tdiff, file=hist)
 
 if __name__ == '__main__':
     main()
